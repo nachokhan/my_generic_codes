e43b907fa9cd75a72185b266e7809a4eefef3975
nachokhan <ignacio.rigoni@gmail.com>
2021-06-02 12:44:15 -0300

    updated file

--- deployment/wsgi.py
+++ deployment/wsgi.py
@@ -1,7 +1,7 @@
 # This file will be used for FLASK apps.
 # Because Django creates its own wsgi.py file
 
-from {{app_module}} import {{app_object_name}} as application
+from {{app_module}} import {{app_object_name}}
 
 if __name__ == '__main__':
-    application.run(debug=False)
+    {{app_object_name}}.run(debug=False)
